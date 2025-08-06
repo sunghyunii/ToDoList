0854b68750b818b65fcdf0f1bf2a0f61d4578a16
sunghyunii <112795295+sunghyunii@users.noreply.github.com>
2025-08-06 11:14:31 +0900

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,2 +1,2 @@
 echo "> 앱 실행"
-nohup java -jar /home/ubuntu/app/build/your-app.jar > /home/ubuntu/app/nohup.out 2>&1 &
+nohup java -jar /home/ubuntu/app/build/todoapp-0.0.1-SNAPSHOT.jar > /home/ubuntu/app/nohup.out 2>&1 &
