91f8c3a48952102acc78a5e25251dddc2e28ff14
sunghyunii <112795295+sunghyunii@users.noreply.github.com>
2025-08-11 23:01:33 +0900

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,3 @@
 echo "> 애플리케이션 실행"
-JAR_NAME=$(ls /home/ubuntu/app/build/*.jar | tail -n 1)
+JAR_NAME=$(ls /home/ubuntu/app/libs/*.jar | tail -n 1)
 nohup java -jar "$JAR_NAME" > /home/ubuntu/app/nohup.out 2>&1 &
